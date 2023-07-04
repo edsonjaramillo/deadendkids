fc1ef7c891a6d3b733d32481fa93f040c1c218ca
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-07-03 19:26:10 -0500

    changed text color for p tags in terms and privacy

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -15,27 +15,27 @@ export default function PrivacyPage() {
   return (
     <div className="responsive-sm space-y-10 py-14">
       <h1 className="text-4xl font-bold">Privacy Policy</h1>
-      <p>
+      <p className="text-base-400">
         This Privacy Policy describes how we collect, use, and disclose your personal information when you use our
         services.
       </p>
       <h2 className="text-3xl font-bold">Information We Collect</h2>
       <ul className="list-disc list-inside text-base-200">
         <li className="text-base text-base-200">
-          <p>
-            <strong>Personal Information:</strong> We may collect certain personally identifiable information, such as
-            your name, email address, and phone number when you voluntarily provide it to us.
+          <p className="text-base-400">
+            <strong className="text-white">Personal Information:</strong> We may collect certain personally identifiable
+            information, such as your name, email address, and phone number when you voluntarily provide it to us.
           </p>
         </li>
         <li className="text-base text-base-200">
-          <p>
-            <strong>Usage Information:</strong> We may collect information about how you interact with our services,
-            including your IP address, device information, and browsing activity.
+          <p className="text-base-400">
+            <strong className="text-white">Usage Information:</strong> We may collect information about how you interact
+            with our services, including your IP address, device information, and browsing activity.
           </p>
         </li>
       </ul>
       <h2 className="text-3xl font-bold">Use of Information</h2>
-      <p>We may use the collected information for the following purposes:</p>
+      <p className="text-base-400">We may use the collected information for the following purposes:</p>
       <ul className="list-disc list-inside text-base-200">
         <li className="text-base text-base-200">To provide and maintain our services to you.</li>
         <li className="text-base text-base-200">To personalize your experience and improve our services.</li>
@@ -49,7 +49,9 @@ export default function PrivacyPage() {
         </li>
       </ul>
       <h2 className="text-3xl font-bold">Disclosure of Information</h2>
-      <p>We may disclose your personal information to third parties in the following circumstances:</p>
+      <p className="text-base-400">
+        We may disclose your personal information to third parties in the following circumstances:
+      </p>
       <ul className="list-disc list-inside text-base-200">
         <li className="text-base text-base-200">
           With your consent or as necessary to fulfill the purpose for which the information was provided.
@@ -65,28 +67,28 @@ export default function PrivacyPage() {
         </li>
       </ul>
       <h2 className="text-3xl font-bold">Data Security</h2>
-      <p>
+      <p className="text-base-400">
         We implement reasonable security measures to protect your personal information from unauthorized access,
         disclosure, or alteration. However, please note that no method of transmission over the internet or electronic
         storage is completely secure.
       </p>
       <h2 className="text-3xl font-bold">Data Retention</h2>
-      <p>
+      <p className="text-base-400">
         We will retain your personal information for as long as necessary to fulfill the purposes outlined in this
         privacy policy, unless a longer retention period is required or permitted by law.
       </p>
       <h2 className="text-3xl font-bold">Your Rights</h2>
-      <p>
+      <p className="text-base-400">
         You have the right to access, update, or delete the personal information we hold about you. If you would like to
         exercise these rights, please contact us using the information provided below.
       </p>
       <h2 className="text-3xl font-bold">Changes to this Privacy Policy</h2>
-      <p>
+      <p className="text-base-400">
         We may update this privacy policy from time to time. The most current version will always be posted on our
         website. We encourage you to review this policy periodically for any changes.
       </p>
       <h2 className="text-3xl font-bold">Contact Us</h2>
-      <p>
+      <p className="text-base-400">
         If you have any questions or concerns about this Privacy Policy, please contact us at{' '}
         <a className="text-base text-primary-500 underline" href="mailto:contact@deadendkidsmusic.com">
           contact@deadendkidsmusic.com
--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -15,63 +15,65 @@ export default function Page() {
   return (
     <div className="responsive-sm space-y-10 py-14">
       <h1 className="text-4xl font-bold">Terms and Conditions</h1>
-      <p>
+      <p className="text-base-400">
         These Terms and Conditions (&quot;Terms&quot;) govern your use of our services. Please read these Terms
         carefully before using our services.
       </p>
       <h2 className="text-3xl font-bold">Acceptance of Terms</h2>
-      <p>
+      <p className="text-base-400">
         By accessing or using our services, you agree to be bound by these Terms. If you do not agree with any part of
         these Terms, you may not use our services.
       </p>
       <h2 className="text-3xl font-bold">Use of Services</h2>
-      <p>
+      <p className="text-base-400">
         You agree to use our services only for lawful purposes and in accordance with these Terms. You must not engage
         in any activity that may interfere with or disrupt the operation of our services.
       </p>
       <h2 className="text-3xl font-bold">Intellectual Property</h2>
-      <p>
+      <p className="text-base-400">
         All intellectual property rights in our services, including but not limited to trademarks, logos, and content,
         belong to us or our licensors. You may not use, reproduce, or distribute any of our intellectual property
         without our prior written consent.
       </p>
       <h2 className="text-3xl font-bold">User Content</h2>
-      <p>
+      <p className="text-base-400">
         You are solely responsible for any content you submit or contribute to our services. By submitting content, you
         grant us a non-exclusive, worldwide, royalty-free license to use, reproduce, modify, and distribute the content
         for the purpose of providing our services.
       </p>
       <h2 className="text-3xl font-bold">Limitation of Liability</h2>
-      <p>
+      <p className="text-base-400">
         To the maximum extent permitted by law, we shall not be liable for any direct, indirect, incidental,
         consequential, or exemplary damages arising out of or in connection with the use of our services. This includes
         but is not limited to damages for loss of profits, data, or other intangible losses.
       </p>
       <h2 className="text-3xl font-bold">Third-Party Links</h2>
-      <p>
+      <p className="text-base-400">
         Our services may contain links to third-party websites or services that are not owned or controlled by us. We
         have no control over and assume no responsibility for the content, privacy policies, or practices of any
         third-party websites or services. You access and use such websites or services at your own risk.
       </p>
       <h2 className="text-3xl font-bold">Modification of Terms</h2>
-      <p>
+      <p className="text-base-400">
         We reserve the right to modify or update these Terms at any time without prior notice. It is your responsibility
         to review these Terms periodically for any changes. By continuing to use our services after the modifications,
         you agree to be bound by the updated Terms.
       </p>
       <h2 className="text-3xl font-bold">Termination</h2>
-      <p>
+      <p className="text-base-400">
         We may terminate or suspend your access to our services immediately, without prior notice or liability, for any
         reason whatsoever, including but not limited to a breach of these Terms.
       </p>
       <h2 className="text-3xl font-bold">Governing Law</h2>
-      <p>
+      <p className="text-base-400">
         These Terms shall be governed by and construed in accordance with the laws of [Jurisdiction]. Any legal action
         or proceeding arising out of or relating to these Terms shall be exclusively brought in the courts of
         [Jurisdiction].
       </p>
       <h2 className="text-3xl font-bold">Contact Us</h2>
-      <p>If you have any questions or concerns about these Terms, please contact us at [email address].</p>
+      <p className="text-base-400">
+        If you have any questions or concerns about these Terms, please contact us at [email address].
+      </p>
     </div>
   );
 }
