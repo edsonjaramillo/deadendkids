bb28882db5990cf97e9dcb138b78b939786bd985
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-07-30 12:58:08 -0500

    added seo metadata to blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,17 +1,26 @@
+import { Metadata } from 'next';
+
 import BlogPostCard from '@/components/blog/BlogPostCard';
 import PageHeader from '@/components/shared/PageHeader';
 import Section from '@/components/shared/Section';
+import { SEO } from '@/utils/SEO';
 import { CMSClient } from '@/utils/cms/CMSClient';
+
+export async function generateMetadata(): Promise<Metadata> {
+  return SEO.setMetadata({
+    title: 'Blog',
+    description: 'Stay Tuned for the Latest Updates and Breaking News from Dead End Kids!',
+    route: '/blog',
+    index: true,
+  });
+}
 
 export default async function BlogPage() {
   const posts = await new CMSClient().getBlogPosts();
 
   return (
     <>
-      <PageHeader
-        header="Blog"
-        subHeader="Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque alias ducimus omnis accusamus animi aliquam?"
-      />
+      <PageHeader header="Blog" subHeader="Stay Tuned for the Latest Updates and Breaking News from Dead End Kids!" />
       <Section header="Latest Blogs">
         <div className="grid grid-cols-1 gap-x-8 gap-y-20 lg:grid-cols-3">
           {posts.map((post) => (
